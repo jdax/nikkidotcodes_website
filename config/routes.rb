c8c08adecd7efb4584fe3654824fbc234fcabd3b
Nikki Murray <nikki.liz.murray@gmail.com>
2017-03-28 20:48:59 -0400

    oops did google verification wrong

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resources :writings, only: [:index, :show]
   resources :about_me, only: :index
 
+  get 'googled2345548c400f224.html' => 'application#googled2345548c400f224.html'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
